6d70f32ab1bc4316a587945384060ec7b579b982
Edison Ferraz <edisonferraz@gmail.com>
2019-02-26 13:12:25 +0100

    Add tests explaining mocha blocks

---
+++ tests/main.spec.js
@@ -0,0 +1,27 @@
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen blabla', () => {
+        // espera que aconteça
+        // Entra de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+
+    context('Case 2', () => {
+      it('should happen blabla', () => {
+        // espera que aconteça
+        // Entra de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+
+      it('should happen mimimi', () => {
+        // espera que aconteça
+        // Entra de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+      });
+    });
+  });
+
+  describe('Method B', () => {});
+});
