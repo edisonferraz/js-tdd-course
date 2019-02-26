fb14b5552c6ff6f5e6c730080f1292e3317d667c
Edison Ferraz <edisonferraz@gmail.com>
2019-02-26 13:20:05 +0100

    Explaining bail, skip and only

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,18 +1,23 @@
+// npm test -- --bail no console para parar no primeiro fail
+
 describe('Main', () => {
   describe('Method A', () => {
     context('Case 1', () => {
-      it('should happen blabla', () => {
+      it.skip('should happen blabla', () => {
         // espera que aconteça
         // Entra de dados / método sum(2,2)
         // Espera retornar (4) => true | (3) => false => broken test
+        throw new Error('just an error');
       });
     });
 
     context('Case 2', () => {
+      // only para rodar só um bloco
       it('should happen blabla', () => {
         // espera que aconteça
         // Entra de dados / método sum(2,2)
         // Espera retornar (4) => true | (3) => false => broken test
+        throw new Error('just an error');
       });
 
       it('should happen mimimi', () => {
