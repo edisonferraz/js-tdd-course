65bd2bfdfd43b546e4a478a361baea43c12b195e
Edison Ferraz <edisonferraz@gmail.com>
2019-02-26 13:31:34 +0100

    Add hooks example

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,30 +1,38 @@
 describe('Main', () => {
+  let arr;
+
   // roda uma vez, antes do bloco
   before(() => {
-    console.log('before');
+    // inicia uma conexão no banco
+    // criar um conjunto de dados
   });
 
   // roda uma vez, depois do bloco
   after(() => {
-    console.log('after');
+    // fecha conexão do banco
+    // apagar esse conjunto de dados
   });
 
   // roda todas as vezes, antes de CADA bloco
   beforeEach(() => {
-    console.log('beforeEach');
+    arr = [1, 2, 3];
   });
 
   // roda todas as vezes, depois de CADA bloco
-  afterEach(() => {
-    console.log('afterEach');
+  afterEach(() => {});
+
+  it('should have a size of 4 when push another value to the array', () => {
+    arr.push(4);
+    console.log(arr.length); // 4
   });
 
-  it('test 1', () => {
-    console.log('test 1');
+  it('should remove the value 3 when use pop in the array', () => {
+    console.log(arr.pop() === 3); // true
   });
 
-  it('test 2', () => {
-    console.log('test 2');
+  it('should have a size of 2 when pop a value from the array', () => {
+    arr.pop();
+    console.log(arr.length); // 2
   });
 });
 
